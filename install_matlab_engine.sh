725cf8c0424f2f9d697113333d448601c5aea209
Tom O'Connell <toconnel@caltech.edu>
2019-01-28 17:13:01 -0800

    Added update to GCC 6.3 required for matlab engine to work, in install script.

--- install_matlab_engine.sh
+++ install_matlab_engine.sh
@@ -10,6 +10,12 @@
 # tom@atlas:~/src/python_2p_analysis$
 # ./install_matlab_engine.sh /usr/local/MATLAB/R2018b
 
+LD_LIBRARY_PATH=/usr/local/lib64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+sudo apt-add-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update
+sudo apt-get install gcc-6 g++-6
+
 matlabroot="$1"
 cd ${matlabroot}/extern/engines/python
 # TODO may need root if in sys dir?
