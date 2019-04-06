58a7885a5931b522fcfe06819d166e0ec35d68fe
Tom O'Connell <toconnel@caltech.edu>
2019-04-06 15:44:49 -0700

    Commented part of matlab engine install script b/c doesn't work w/ anaconda.

--- install_matlab_engine.sh
+++ install_matlab_engine.sh
@@ -14,9 +14,12 @@ LD_LIBRARY_PATH=/usr/local/lib64/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 sudo apt-add-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
-sudo apt-get install gcc-6 g++-6
+sudo apt-get install gcc-6 g++-6 -y
 
-matlabroot="$1"
-cd ${matlabroot}/extern/engines/python
-# TODO may need root if in sys dir?
-sudo python3 setup.py install
+# TODO support installing in anaconda, which seems to require chmod -R 777
+# on MATLAB install dir (build at least), then calling setup.py w/ anaconda
+# python as non-root user
+#matlabroot="$1"
+#cd ${matlabroot}/extern/engines/python
+## TODO may need root if in sys dir?
+#sudo python3 setup.py install
