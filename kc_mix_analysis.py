742314e2bacc11a3ebc28ed1512bb42995196e10
Tom O'Connell <toconnel@caltech.edu>
2019-10-08 01:17:05 -0700

    Added ROC analysis and pca plots. Saving them.

--- kc_mix_analysis.py
+++ kc_mix_analysis.py
@@ -6,10 +6,14 @@ import glob
 from pprint import pprint as pp
 import warnings
 import time
+import pickle
 
 import numpy as np
 #from scipy.optimize import minimize
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import seaborn as sns
@@ -29,8 +33,8 @@ odor_and_fit_matrices = False
 # + eag too
 avg_traces = True
 
-#test = False
-test = True
+test = False
+#test = True
 
 fig_dir = 'mix_figs'
 if not exists(fig_dir):
@@ -120,6 +124,186 @@ def odor_and_fit_plot(odor_cell_stats, weighted_sum, ordered_cells, fname,
     f3.savefig(join(fig_dir, 'png', 'odorandfit_' + fname + '.png'))
     f3.savefig(join(fig_dir, 'svg', 'odorandfit_' + fname + '.svg'))
 
+
+# TODO add appropriately formatted descrip of mix to input index cols so it can
+# be used in plotting
+def plot_pca(df, fname=None):
+    pca_unstandardized = True
+    if pca_unstandardized:
+        pca_2 = PCA(n_components=2)
+        pca_data = pca_2.fit_transform(df)
+
+        pca_data = pd.DataFrame(index=df.index, data=pca_data)
+        pca_data.rename(columns={0: 'PC1', 1: 'PC2'}, inplace=True)
+
+        f1a = plt.figure()
+        sns.scatterplot(data=pca_data.reset_index(), x='PC1', y='PC2',
+            hue='name1', legend='full')
+        #    hue='sample_type', style='fermented', size='day', legend='full')
+        plt.title('PCA on raw data')
+
+        pca_obj = PCA()
+        pca_obj.fit(df)
+
+        f1b = plt.figure()
+        # TODO is explained_variance_ratio_ what i want exactly?
+        plt.plot(pca_obj.explained_variance_ratio_[:15], 'k.')
+        plt.title('PCA on raw data')
+        plt.xlabel('Principal component')
+        plt.ylabel('Fraction of explained variance')
+
+        '''
+        pc_df = pd.DataFrame(columns=df.columns,
+                             data=pca_obj.components_)
+
+        ###pc_df.rename(columns=fmt_chem_id, inplace=True)
+        ###pc_df.columns.name = 'Chemical'
+        for pc in range(2):
+            print('Unstandardized PC{}:'.format(pc))
+            # TODO check again that this is doing what i want
+            # (factor to fn too)
+            print(pc_df.iloc[pc].abs().sort_values(ascending=False)[:10])
+        '''
+
+    standardizer = StandardScaler()
+    df_standardized = standardizer.fit_transform(df)
+
+    pca_obj = PCA()
+    pca_obj.fit(df_standardized)
+
+    pca_2 = PCA(n_components=2)
+    pca_data = pca_2.fit_transform(df_standardized)
+
+    for n in range(pca_2.n_components):
+        assert np.allclose(pca_obj.components_[n], pca_2.components_[n])
+
+    # From Wikipedia page on PCA:
+    # "If there are n observations with p variables, then the number of
+    # distinct principal components is min(n - 1, p)."
+    assert len(df.columns) == pca_2.n_features_
+    assert len(df.index) == pca_2.n_samples_
+
+    pca_data = pd.DataFrame(index=df.index, data=pca_data)
+    pca_data.rename(columns={0: 'PC1', 1: 'PC2'}, inplace=True)
+
+    f2a = plt.figure()
+    # TODO plot trajectories instead of using marker size to indicate time
+    # point
+    # TODO what about day? size? (saturation / alpha would be ideal i think)
+    sns.scatterplot(data=pca_data.reset_index(), x='PC1', y='PC2',
+        hue='name1', legend='full')
+        #hue='sample_type', style='fermented', size='day')#, legend='full')
+
+    plt.title('PCA on standardized data')
+    # TODO save figure
+
+    f2b = plt.figure()
+    plt.plot(pca_obj.explained_variance_ratio_[:15], 'k.')
+    plt.title('PCA on standardized data')
+    plt.xlabel('Principal component')
+    plt.ylabel('Fraction of explained variance')
+
+    '''
+    pc_df = pd.DataFrame(columns=df.columns, data=pca_obj.components_)
+    ####pc_df.rename(columns=fmt_chem_id, inplace=True)
+    ####pc_df.columns.name = 'Chemical'
+    for pc in range(2):
+        print('Standardized PC{}:'.format(pc))
+        print(pc_df.iloc[pc].abs().sort_values(ascending=False)[:10])
+    '''
+
+    if fname is not None:
+        f1a.savefig(join(fig_dir, 'png', 'pca_unstandardized_' + fname + '.png'))
+        f1a.savefig(join(fig_dir, 'svg', 'pca_unstandardized_' + fname + '.svg'))
+        f1b.savefig(join(fig_dir, 'png', 'skree_unstandardized_' + fname + '.png'))
+        f1b.savefig(join(fig_dir, 'svg', 'skree_unstandardized_' + fname + '.svg'))
+        f2a.savefig(join(fig_dir, 'png', 'pca_' + fname + '.png'))
+        f2a.savefig(join(fig_dir, 'svg', 'pca_' + fname + '.svg'))
+        f2b.savefig(join(fig_dir, 'png', 'skree_' + fname + '.png'))
+        f2b.savefig(join(fig_dir, 'svg', 'skree_' + fname + '.svg'))
+
+
+# TODO maybe try on random subsets of cells (realistic # that MBONs read out?)
+# TODO TODO count mix as same class or not? both?
+# how do shen et al. disentagle this again? their generalization etc figures?
+# TODO TODO use metric other than AU(roc)C since imbalanced classes
+# area under ~"precision-recall" curve?
+def roc_analysis(window_trial_stats, reliable_responders, fname=None):
+    # TODO maybe don't subset to "reliable" responders as shen et al do?
+    # TODO maybe just two distributions, one for natural one control?
+    odors = window_trial_stats.index.get_level_values('name1')
+    df = window_trial_stats[odors != 'pfo'].reset_index()
+
+    auc_dfs = []
+    for segmentation in (True, False):
+        for odor in df.name1.unique():
+            if segmentation and (odor == 'mix' or odor == 'kiwi'):
+                continue
+
+            odor_reliable_responders = reliable_responders.loc[(odor,)]
+            odor_reliable_cells = odor_reliable_responders[
+                odor_reliable_responders].index
+
+            cells = []
+            aucs = []
+            for gn, gdf in df[df.cell.isin(odor_reliable_cells)
+                ].groupby('cell'):
+
+                if segmentation:
+                    gdf = gdf[gdf.name1 != 'kiwi']
+
+                if segmentation:
+                    labels = (gdf.name1 == 'mix') | (gdf.name1 == odor)
+                else:
+                    labels = gdf.name1 == odor
+
+                scores = gdf.df_over_f
+                fpr, tpr, thresholds = roc_curve(labels, scores)
+                curr_auc = auc(fpr, tpr)
+                cells.append(gn)
+                aucs.append(curr_auc)
+
+            auc_dfs.append(pd.DataFrame({
+                'task': 'segmentation' if segmentation else 'discrimination',
+                'odor': odor,
+                'cell': cells,
+                'auc': aucs
+            }))
+
+            fig = plt.figure()
+            plt.hist(aucs, range=(0,1))
+            plt.axvline(x=0.5, color='r')
+            # TODO TODO TODO add stuff to identify recording to titles
+            if segmentation:
+                plt.title('AUC distribution, segmentation for {}'.format(odor))
+            else:
+                plt.title('AUC distribution, {} vs all discrimination'.format(
+                    odor))
+
+            if fname is not None:
+                odor_fname = odor.replace(' ', '_')
+                task_fname = 'seg' if segmentation else 'discrim'
+
+                fig.savefig(join(fig_dir, 'png', fname + '_' + odor_fname + '_'
+                    + task_fname + '.png'))
+
+                fig.savefig(join(fig_dir, 'svg', fname + '_' + odor_fname + '_'
+                    + task_fname + '.svg'))
+
+    # TODO maybe also plot average distributions for segmentation and
+    # discrimination, within this fly, but across all odors?
+    # (if so, should odors be weighted equally, or should each cell (of which
+    # some odors will have less b/c less reliable responders)?)
+
+    #plt.show()
+    # TODO TODO savefigs
+
+    # TODO TODO also return auc values (+ n cells used in each case?) for
+    # analysis w/ data from other flies
+    auc_df = pd.concat(auc_dfs, ignore_index=True)
+    return auc_df
+
+
 pickle_dir = '/mnt/nas/mb_team/analysis_output/trace_pickles'
 glob_strs = ['20190815*.p', '20190909*.p']
 pickles = []
@@ -149,12 +333,15 @@ max_plotting_td = 1.0
 
 cell_cols = ['name1','name2','repeat_num','cell']
 response_calling_s = 5.0
-dfs = []
+responder_frac_dfs = []
+auc_dfs = []
 for df_pickle in pickles:
     # TODO also support case here pickle has a dict
     # (w/ this df behind a trace_df key & maybe other data, like PID, behind
     # something else?)
     df = pd.read_pickle(df_pickle)
+    # TODO resolve whatever caused recording_from to get split into _x and _y
+    # cols (seems to be same values though)
 
     assert 'original_name1' in df.columns
     df.name1 = df.original_name1.map(cu.odor2abbrev)
@@ -179,10 +366,16 @@ for df_pickle in pickles:
     '''
     prefix = u.df_to_odorset_name(df)
     odor_order = [cu.odor2abbrev(o) for o in u.df_to_odor_order(df)]
+    # TODO delete this hack
+    if 'mix' not in odor_order:
+        odor_order = odor_order + ['mix']
+    #
     
     title = '/'.join([x for x in df_pickle[:-2].split('_')[-4:] if len(x) > 0])
     fname = prefix.replace(' ','') + '_' + title.replace('/','_')
     title = prefix.title() + ': ' + title
+
+    print(fname)
 
     # TODO maybe convert other handling of from_onset to timedeltas?
     # (also for ease of resampling / using other time based ops)
@@ -194,13 +387,99 @@ for df_pickle in pickles:
 
     window_df = df.loc[in_response_window,
         cell_cols + ['order','from_onset','df_over_f']]
+    window_df.set_index(cell_cols + ['order','from_onset'], inplace=True)
+
     window_by_trial = window_df.groupby(cell_cols + ['order'])['df_over_f']
+
+    # TODO use a longer/shorter baseline window?
+    in_baseline_window = ((df.from_onset >= pd.Timedelta(-2, unit='s')) &
+        (df.from_onset <= pd.Timedelta(0, unit='s')))
 
     stat = 'max'
     if stat == 'max':
         window_trial_stats = window_by_trial.max()
     elif stat == 'mean':
         window_trial_stats = window_by_trial.mean()
+
+    baseline_df = df.loc[in_baseline_window,
+        cell_cols + ['order','from_onset','df_over_f']]
+    baseline_by_trial = baseline_df.groupby(cell_cols + ['order'])['df_over_f']
+
+    '''
+    # TODO could just groupby once and then pass multiple functions to
+    # agg?
+    baseline_stddev = baseline_by_trial.std()
+    baseline_mean = baseline_by_trial.mean()
+    response_criteria = pd.Series(index=window_df.index)
+    # TODO maybe z change should be computed from raw f rather than dff?
+    # just calc ratio over stddev with this metric?
+    for gn, gdf in window_df.groupby(cell_cols + ['order']):
+        zchange = ((gdf.df_over_f - baseline_mean.loc[gn]) /
+            baseline_stddev.loc[gn])
+        response_criteria.loc[gn] = zchange
+
+    # TODO not sure why it seems i need such a high threshold here, to get
+    # reasonable sparseness... was the fly i'm testing it with just super
+    # responsive?
+    mean_zchange_response_thresh = 4.0
+    trial_responders = (response_criteria.groupby(cell_cols).agg('mean') >
+        mean_zchange_response_thresh)
+    '''
+    # picked from histogram on one fly's data
+    max_responder_thresh = 0.7
+    trial_responders = window_by_trial.max() > max_responder_thresh
+    # TODO exclude pfo for this and below? (probably for reported means)
+    print(('Fraction of cells trials counted as responses (max dff > {}): '
+        '{:.3f}').format(max_responder_thresh, 
+        trial_responders.sum() / len(trial_responders)))
+
+    # As >= 50% response to odor criteria in Shen paper
+    reliable_responders = trial_responders.groupby(['name1','cell']).sum() >= 2
+    print(('Mean fraction of cells responding to at least 2/3 trials: {:.3f}'
+        ).format(reliable_responders.sum() / len(reliable_responders)))
+
+    n_cells = len(df.cell.unique())
+    print('Fraction of cells responding to each trial of each odor:')
+    # TODO TODO is the population becoming silent to kiwi more quickly? is that
+    # consistent?
+    frac_odor_trial_responders = (trial_responders.groupby(['name1','repeat_num'
+        ]).sum() / n_cells)
+    print(frac_odor_trial_responders.to_string(float_format='%.3f'))
+
+    mean_frac_odor_responders = \
+        frac_odor_trial_responders.groupby('name1').mean()
+
+    print('Mean fraction of cells responding to each odor:')
+    print(mean_frac_odor_responders.to_string(float_format='%.3f'))
+    # TODO TODO also concat these responder fraction things across flies
+    # w/ appropriate index info -> make plots after loop over data
+
+    # TODO also breakdown reliable by odor?
+
+    # TODO TODO concat across recordings, w/ appropriate index info added
+    auc_df = roc_analysis(window_trial_stats, reliable_responders, fname=fname)
+
+    def get_single_index_val(var):
+        values  = df[var].unique()
+        assert len(values) == 1
+        return values[0]
+
+    def add_metadata(out_df):
+        for var in ('prep_date', 'fly_num', 'thorimage_id'):
+            out_df[var] = get_single_index_val(var)
+        return out_df
+
+    responder_frac_dfs.append(add_metadata(trial_responders))
+    auc_dfs.append(add_metadata(auc_df))
+
+    do_pca = True
+    if do_pca:
+        pivoted_window_trial_stats = pd.pivot_table(
+            window_trial_stats.to_frame(name=stat), columns='cell',
+            index=['name1','name2','repeat_num'], values=stat
+        )
+        # TODO TODO add stuff to identify recording to titles
+        plot_pca(pivoted_window_trial_stats, fname=fname)
 
     responsiveness = window_trial_stats.groupby('cell').mean()
     cellssorted = responsiveness.sort_values(ascending=False)
@@ -243,7 +522,12 @@ for df_pickle in pickles:
         if x not in ('mix', 'kiwi', 'pfo')] 
     # TODO also do on traces as in kc_analysis?
     # or at least per-trial rather than per-mean?
-    mix = odor_cell_stats.loc['mix']
+
+    # TODO delete try/except
+    try:
+        mix = odor_cell_stats.loc['mix']
+    except KeyError:
+        import ipdb; ipdb.set_trace()
 
     components = odor_cell_stats.loc[component_names]
     component_sum = components.sum()
@@ -541,5 +825,18 @@ for df_pickle in pickles:
             odor_and_fit_plot(odor_cell_stats, weighted_sum, order[:top_n],
                 fname + ss, title, sort_odor_labels, cbar_label)
 
+    print('\n')
+
+responder_frac_df = pd.concat(responder_frac_dfs, ignore_index=True)
+auc_df = pd.concat(auc_dfs, ignore_index=True)
+
+with open('kc_mix_analysis_outputs.p', 'wb') as f:
+    data = {
+        'responder_frac_df': responder_frac_df,
+        'auc_df': auc_df
+    }
+    pickle.dump(data, f)
+
 plt.show()
+import ipdb; ipdb.set_trace()
 
