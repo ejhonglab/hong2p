ad349e8fc6bbb380d3c620f473b9732fd07eaeab
Tom O'Connell <toconnel@caltech.edu>
2020-05-19 08:31:56 -0700

    Work on antennal calibration analysis script. Maybe incomplete.

--- scripts/antennal_volumetric_calibration.py
+++ scripts/antennal_volumetric_calibration.py
@@ -10,22 +10,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import cv2
 
 import hong2p.util as u
 
 
-def main():
-    '''
-    keys_list = [
-        ('2020-03-09', 1, 'fn_007'),
-        ('2020-03-09', 1, 'fn_008'),
-        ('2020-03-09', 1, 'fn_009'),
-        ('2020-03-09', 2, 'fn'),
-        ('2020-03-09', 2, 'fn_001'),
-        ('2020-03-09', 2, 'fn_002'),
-    ]
-    for ks in keys_list:
+# TODO maybe rename now that it also has antennal data...
+gdf = u.mb_team_gsheet()
+gdf = gdf[gdf.stimulus_data_file.apply(lambda x: '_vv-' in x) &
+    gdf.attempt_analysis
+]
+# Assuming there are no non-calibration pickles on the same dates as 
+# calibration experiments (that have their own trace pickles).
+date_strs = [u.format_date(d) for d in gdf.date.unique()]
+
+def write_trace_pickles():
+    keys = ['date', 'fly_num', 'thorimage_dir']
+    for ks, group_df in gdf.groupby(keys):
         data_dir = u.thorimage_dir(*ks)
+        # TODO assert the dir exists?
 
         avg_tiff_fname = join(data_dir, 'avg.tif')
         if not exists(avg_tiff_fname):
@@ -42,37 +45,67 @@ def main():
             print(f'writing average to {avg_tiff_fname}')
             u.write_tiff(avg_tiff_fname, avg)
 
+            import ipdb; ipdb.set_trace()
+
+            # TODO TODO also write rois from threshold (+ dilation)?
+
             print('You must save ImageJ ROIs to rois.zip in same folder before'
                 'further analysis on this recording will be done.'
             )
             continue
 
+        # TODO TODO TODO check trace pickle doesn't already exist
+        # (unless maybe a flag to regenerate them...)
+        import ipdb; ipdb.set_trace()
+
         fly_dir, thorimage_id = split(data_dir)
         tiff = join(fly_dir, 'tif_stacks', f'{thorimage_id}.tif')
         assert exists(tiff)
+        # Calling this for its side effect of writing the "trace" pickle for the
+        # current recording.
         data = u.load_recording(tiff)
-    '''
-    trace_pickle_dir = '/mnt/nas/mb_team/analysis_output/trace_pickles'
-    pickles = glob.glob(join(trace_pickle_dir, '*_2020-03-09*.p'))
+
+
+def process_trace_pickles():
+    trace_pickle_dir = join(u.analysis_output_root(), 'trace_pickles')
+    # TODO TODO TODO if enumerating thorimage dirs above from a list of dates,
+    # use same dates here to glob the pickles
+    pickles = [
+        glob.glob(join(trace_pickle_dir, f'*_{s}*.p')) for s in date_strs
+    ]
+    print(pickles)
+    import ipdb; ipdb.set_trace()
+    #pickles = glob.glob(join(trace_pickle_dir, '*_2020-03-09*.p'))
     dfs = []
     for p in pickles:
         pdf = pd.read_pickle(p)
         dfs.append(pdf)
-
     df = pd.concat(dfs)
 
+    # This is to exclude landmark odors (those intended to just activate
+    # a single glomerulus).
     df = df[~ df.original_name1.isin(('2,3-butanedione', 'ammonia',
         'ethyl acetate', 'trans-2-hexenal', 'pfo', '2-butanone'))
     ]
 
     keys = ['fly_num', 'original_name1', 'log10_conc_vv1']
     dff_max = df.groupby(keys).df_over_f.max().reset_index()
+    import ipdb; ipdb.set_trace()
 
-    '''
+    #'''
     g = sns.FacetGrid(dff_max, row='original_name1', hue='fly_num', sharey=False)
     g.map(plt.plot, 'log10_conc_vv1', 'df_over_f')
+    # TODO savefig
     plt.show()
-    '''
+
+    return df
+
+
+def main():
+    # TODO probably just provide dates and have something enumerate thorimage
+    # dirs from that?
+    #write_trace_pickles()
+    df = process_trace_pickles()
     import ipdb; ipdb.set_trace()
 
 
