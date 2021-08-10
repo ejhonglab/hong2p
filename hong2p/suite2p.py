504387c7f9c0db8365827784abe93b3b9626cc18
Tom O'Connell <toconnel@caltech.edu>
2021-08-10 14:20:04 -0700

    Refactor print_suite2p_params to split out a suite2p_params fn, which can compute the same values for use in calling suite2p automatically. Add/remove TODOs

--- hong2p/suite2p.py
+++ hong2p/suite2p.py
@@ -5,15 +5,13 @@ Functions for working with suite2p as part of analysis.
 from hong2p import thor
 
 
-def print_suite2p_params(thorimage_dir):
+def suite2p_params(thorimage_dir):
     # From: https://suite2p.readthedocs.io/en/latest/settings.html
 
     single_plane_fps, xy, z, c, n_flyback, _ = thor.load_thorimage_metadata(
         thorimage_dir
     )
 
-    # TODO TODO TODO is this per plane or per volume in the volumetric case? might need
-    # to read code...
     # "(int, default: 1) each tiff has this many planes in sequence"
     # wait, is this =z, or =z*t or just =t?
     nplanes = z
@@ -30,17 +28,31 @@ def print_suite2p_params(thorimage_dir):
     # data. We recommend: 0.7 for GCaMP6f, 1.0 for GCaMP6m, 1.25-1.5 for GCaMP6s"
     #tau = 
 
-    # TODO TODO TODO is this per plane or per volume in the volumetric case? might need
-    # to read code...
     # "(float, default: 10.0) Sampling rate (per plane). For instance, if you have a 10
     # plane recording acquired at 30Hz, then the sampling rate per plane is 3Hz,
     # so set ops[‘fs’] = 3 "
     fs = single_plane_fps / (z + n_flyback)
 
+    # TODO TODO how much of a problem is it to ignore the fact that there are flyback
+    # frames contributing to time before a plane is returned to? not sure i'm
+    # understanding 'fs' correctly to begin with...
+
+    # TODO TODO maybe use ops['ignore_flyback'] (== indices of each flyback frame?
+    # or is there a more concise option?) to make fs more meaningful?
+
     # TODO TODO can we pick a better spatial_scale than suite2p does automatically?
     # does its optimal value even differ across the types of data we have in the lab?
 
-    print('nplanes:', nplanes, '(assuming nplanes == z)')
+    ops = {
+        'fs': fs,
+        'nplanes': nplanes,
+    }
+    return ops
+
+
+def print_suite2p_params(thorimage_dir):
+    ops = suite2p_params(thorimage_dir)
+    print('nplanes:', ops['nplanes'])
+    print(f'fs: {ops["fs"]:.2f}')
     print('tau: 0.7 (recommended for GCaMP6f)')
-    print('fs:', fs)
 
