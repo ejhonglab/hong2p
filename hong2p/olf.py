6b2957e986a129f61dab9a666c41d0014293ea9e
Tom O'Connell <toconnel@caltech.edu>
2022-05-17 20:08:21 -0700

    olf: move sort_odor_indices and odor_index_sort_key there from al_analysis

--- hong2p/olf.py
+++ hong2p/olf.py
@@ -10,6 +10,7 @@ from collections import Counter
 import warnings
 from typing import Union, Sequence, Optional
 
+import numpy as np
 import pandas as pd
 
 
@@ -42,6 +43,107 @@ def sort_odor_list(odor_list):
     be used assuming equal length of all)
     """
     return sorted(odor_list, key=odordict_sort_key)
+
+
+# TODO share (parts?) w/ similar key fn now in hong2p.olf?
+# TODO document i/o types
+def odor_index_sort_key(level, name_order=None):
+    # The assignment below failed for some int dtype levels, even though the boolean
+    # mask dictating where assignment should happen must have been all False...
+    if level.dtype != np.dtype('O'):
+        return level
+
+    sort_key = level.values.copy()
+
+    solvent_elements = sort_key == solvent_str
+    conc_delimiter = '@'
+    assert all([conc_delimiter in x for x in sort_key[~ solvent_elements]])
+
+    # TODO share this + fn wrapping this that returns sort key w/ other place that
+    # is using sorted(..., key=...)
+    def parse_log10_conc(odor_str):
+        assert conc_delimiter in odor_str
+        parts = odor_str.split(conc_delimiter)
+        assert len(parts) == 2
+        return float(parts[1].strip())
+
+    def parse_odor_name(odor_str):
+        assert conc_delimiter in odor_str
+        parts = odor_str.split(conc_delimiter)
+        assert len(parts) == 2
+        # TODO want to handle 'pfo'/'solvent' special?
+        return parts[0].strip()
+
+    if not all(solvent_elements):
+        conc_keys = [parse_log10_conc(x) for x in sort_key[~ solvent_elements]]
+        sort_key[~ solvent_elements] = conc_keys
+
+        # Setting solvent to an unused log10 concentration just under lowest we have,
+        # such that it gets sorted into overall order as I want (first, followed by
+        # lowest concentration).
+        # TODO TODO maybe just use float('-inf') or numpy equivalent here?
+        # should give me the sorting i want.
+        sort_key[solvent_elements] = min(conc_keys) - 1
+
+        if name_order is not None:
+            if solvent_elements.sum() > 0:
+                raise NotImplementedError("not currently supporting 'solvent' when "
+                    'using name_order keyword argument'
+                )
+
+            # TODO TODO TODO do i need to handle solvent_elements here? how?
+            # possible, or will i just need to do outside of this within-level-only
+            # function?
+            names = [parse_odor_name(x) for x in level]
+
+            if not all(n in name_order for n in names):
+                # TODO only print the ones missing
+                raise ValueError(
+                    f'some of names={names} were not in name_order={name_order}'
+                )
+
+            name_orders = [name_order.index(x) for x in names]
+
+            sort_key = list(zip(name_orders, sort_key))
+
+            # tupleize_cols=False prevents a MultiIndex from being created
+            index = pd.Index(sort_key, tupleize_cols=False, name=level.name)
+            return index
+
+    # Converting back to an index so that `level=<previous level name>` arg to
+    # `DataFrame.sort_index` doesn't get broken. This key function is used to generate
+    # an intermediate Index pandas uses to sort, and that intermediate needs to have the
+    # same level names to be able to refer to them as if it was the input object.
+    return pd.Index(sort_key, name=level.name)
+
+
+# TODO TODO add some kind of lookup for odor panels (might just need to get the set of
+# all (odor name, odor concentrations) used in experiment and compare that.  -> force
+# consistent order for things like kiwi.yaml/control1.yaml experiments (anything not
+# pair that we actually wanna see plots for actually. probably just don't wanna sort
+# glomeruli diagnostics) (only really relevant if i actually start randomizing order in
+# those experiments... for now, could just not sort)
+# TODO TODO TODO allow passing in an odor order for the odor names, used in conjunction
+# with the sorting on the concentrations
+odor_cols = ['odor1', 'odor2', 'odor1_b', 'odor2_b']
+def sort_odor_indices(df: pd.DataFrame, name_order=None) -> pd.DataFrame:
+
+    def levels_to_sort(index):
+        return [k for k in index.names if k in odor_cols]
+
+    levels = levels_to_sort(df.index)
+    for axis_name in ('index', 'columns'):
+        levels = levels_to_sort(getattr(df, axis_name))
+        if len(levels) > 0:
+            # TODO check my level sort key fn works in both case of 1 level passed in as
+            # well as 2
+            df = df.sort_index(
+                key=lambda x: odor_index_sort_key(x, name_order=name_order),
+                axis=axis_name, level=levels,
+                sort_remaining=False
+            )
+
+    return df
 
 
 def yaml_data2pin_lists(yaml_data):
@@ -140,6 +242,10 @@ def format_odor(odor_dict, conc=True, name_conc_delim=' @ ', conc_key='log10_con
     return ostr
 
 
+# TODO TODO decorator or some other place to store minimum set of keys (and types?) for
+# these formatting functions (at least those that take a Series as one option)?
+# (so that hong2p.viz.callable_ticklabels can automatically convert / make good error
+# messages if they are missing)
 def format_mix_from_strs(odor_strs: Union[Sequence[str], pd.Series],
     delim: Optional[str] = None):
 
